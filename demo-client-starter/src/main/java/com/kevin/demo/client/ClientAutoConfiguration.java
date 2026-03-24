460c28da70c4ae361c7719988d89e20aaa6a0d38
王琪 <wangqi2017@xinhua.org>
2026-03-24 15:53:42 +0800

    将Resilience4j的配置抽取到client-starter公共模块中，考虑到springboot多模块配置加载优先级问题，只允许加载一个application.yml文件且多个application.yml文件只会覆盖不会合并，因此使用.properties文件

--- demo-client-starter/src/main/java/com/kevin/demo/client/ClientAutoConfiguration.java
+++ demo-client-starter/src/main/java/com/kevin/demo/client/ClientAutoConfiguration.java
@@ -3,10 +3,12 @@ package com.kevin.demo.client;
 import com.kevin.demo.client.echo.EchoClient;
 import com.kevin.demo.client.user.UserClient;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @AutoConfiguration
 @ImportHttpServices(group = "demo-echo-service", types = { EchoClient.class })
 @ImportHttpServices(group = "demo-user-service", types = { UserClient.class })
+@PropertySource("classpath:client-common.properties")
 public class ClientAutoConfiguration {
 }
