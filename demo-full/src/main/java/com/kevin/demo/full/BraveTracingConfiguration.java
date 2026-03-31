dabdb7d1a2d97f8fa81fcacab81c021954cc1475
王琪 <wangqi2017@xinhua.org>
2026-03-31 17:01:18 +0800

    如果用Spring MVC，import org.springframework.http.server.observation.ServerRequestObservationContext。如果用 Spring WebFlux，import org.springframework.http.server.reactive.observation.ServerRequestObservationContext。 通过 ObservationPredicate 可以过滤掉 /actuator 请求，保证 Brave 保持 traceId 全链路一致，同时避免无意义的链路数据。

---
+++ demo-full/src/main/java/com/kevin/demo/full/BraveTracingConfiguration.java
@@ -0,0 +1,35 @@
+package com.kevin.demo.full;
+
+import io.micrometer.observation.ObservationPredicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author 王琪
+ * @date 2026/3/31 16:58
+ */
+@Configuration
+public class BraveTracingConfiguration {
+
+    /**
+     * 不记录 /actuator 下的请求
+     */
+    @Bean
+    public ObservationPredicate actuatorServerContextPredicate() {
+        return (name, context) -> {
+            if (context instanceof org.springframework.http.server.observation.ServerRequestObservationContext serverContext) {
+                // Servlet 环境
+                String uri = serverContext.getCarrier().getRequestURI();
+                return !uri.startsWith("/actuator");
+            } else if (context instanceof org.springframework.http.server.reactive.observation.ServerRequestObservationContext serverContext) {
+                // WebFlux 环境
+                String uri = serverContext.getCarrier().getURI().getPath();
+                return !uri.startsWith("/actuator");
+            }
+            return true;
+        };
+    }
+}
+
+
